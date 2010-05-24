edd04da9d8484f4f5d01607a8e85403842a285a7
jdpace <jared@codewordstudios.com>
2010-05-24 16:53:01 -0400

    Remove references to files named PDFKit vs pdfkit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'PDFKit'
+require 'pdfkit'
 require 'spec'
 require 'spec/autorun'
 
