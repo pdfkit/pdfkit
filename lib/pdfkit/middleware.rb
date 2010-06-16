ff8328441686190ae006e8c4d4314470da32c258
Robby Colvin <geetarista@gmail.com>
2010-06-16 08:41:40 -0700

    Correctly call out relative/absolute. Fix relative URL regex to match single quotes as well.

--- lib/pdfkit/middleware.rb
+++ lib/pdfkit/middleware.rb
@@ -44,11 +44,11 @@ class PDFKit
         uri += '/' unless uri.match(/\/$/)
         root = env['rack.url_scheme'] + "://" + env['HTTP_HOST']
         
-        # translate relative urls
+        # translate absolute urls
         body.gsub!(/(href|src)=['"]([^\/][^\"']*)['"]/,'\1="'+root+'/\2"')
         
-        # translate absolute urls
-        body.gsub!(/(href|src)=['"]\/([^\"]*|[^']*)['"]/,'\1="'+uri+'\2"')
+        # translate relative urls
+        body.gsub!(/(href|src)=['"]\/([^\"']*|[^"']*)['"]/,'\1="'+uri+'\2"')
       end
     
       def set_request_to_render_as_pdf(env)
---
+++ spec/middleware_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe PDFKit::Middleware do
+  describe "#translate_paths" do
+    it "should correctly parse valid absolute url" do
+      #
+    end
+  end
+end
