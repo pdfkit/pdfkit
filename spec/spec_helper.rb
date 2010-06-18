444077f4290c3977f21ac3b6787ac7f97d5f9e44
Jared Pace <jared@codewordstudios.com>
2010-06-18 09:44:20 -0400

    Actually run specs under RSpec 2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,14 +2,16 @@ SPEC_ROOT = File.dirname(__FILE__)
 $LOAD_PATH.unshift(SPEC_ROOT)
 $LOAD_PATH.unshift(File.join(SPEC_ROOT, '..', 'lib'))
 require 'pdfkit'
-require 'spec'
-require 'spec/autorun'
+require 'rspec'
+require 'rspec/autorun'
 require 'mocha'
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
   
   config.before do
-    PDFKit.any_instance.stubs(:wkhtmltopdf).returns(File.join(SPEC_ROOT,'..','bin','wkhtmltopdf-proxy'))
+    PDFKit.any_instance.stubs(:wkhtmltopdf).returns(
+      File.join(SPEC_ROOT,'..','bin','wkhtmltopdf-proxy')
+    )
   end
   
 end
