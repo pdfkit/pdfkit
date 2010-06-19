7919b3d25d2345f5366961dd0b791a384c15cdcb
grosser <grosser.michael@gmail.com>
2010-06-19 14:48:12 +0800

    fix spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 SPEC_ROOT = File.dirname(__FILE__)
 $LOAD_PATH.unshift(SPEC_ROOT)
 $LOAD_PATH.unshift(File.join(SPEC_ROOT, '..', 'lib'))
-require 'pdfkit'
+require 'PDFKit'
 require 'rspec'
 require 'rspec/autorun'
 require 'mocha'
