6581a4aa63be00555b8ad7c8b4abb40dce46814e
Frank Pimenta <frankapimenta@gmail.com>
2012-06-24 23:15:34 +0200

    + generator: specs -> -self.default_directory_path - returns the path of the directory where generated pdf will be saved

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -11,4 +11,25 @@ describe PDFKit::Generator do
     @header_path      = File.join @tmp_dir_path, 'header_support_file.html'
     @footer_path      = File.join @tmp_dir_path, 'footer_support_file.html'
   end
+  describe "#default_directory_path" do
+    before(:each) do # because we are testing class methods with cache ;)
+        PDFKit.send(:remove_const, 'Generator')
+        load './lib/pdfkit/generator.rb'
+    end
+    it "should return the default directory path" do
+      pdfkit_generator_class.send(:default_directory_path).should == @default_dir_path
+    end
+    it "should return the path from PDFKit configurations in initializer" do
+      _pdfkit_configuration_default_path = File.join('documents2')
+      pdfkit_configurations.stub!(:[], :default_directory_path).and_return(_pdfkit_configuration_default_path)
+      # cant use subject because method was called in a previous test and so cache is set
+      pdfkit_generator_class.default_directory_path.should == _pdfkit_configuration_default_path
+    end
+    it "should cache the path used by pdf" do
+      File.should_receive(:join).once.with('documents').and_return(@default_dir_path)
+      pdfkit_generator_class.send(:default_directory_path).should == @default_dir_path
+      # second call to test cache
+      pdfkit_generator_class.send(:default_directory_path).should == @default_dir_path
+    end
+  end
 end
