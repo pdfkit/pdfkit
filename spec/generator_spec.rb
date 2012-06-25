20dc6913b57b6d7152248ad8422968c58ae67aca
Frank Pimenta <frankapimenta@gmail.com>
2012-06-25 23:50:15 +0200

    % generator: specs -> support methods passed to generator_spec_helper.rb

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'generator_spec_helper'
 
 describe PDFKit::Generator do
   let(:pdfkit_generator_class)  {PDFKit::Generator}
@@ -10,66 +11,6 @@ describe PDFKit::Generator do
     @cover_path       = File.join @tmp_dir_path, 'cover_support_file.html'
     @header_path      = File.join @tmp_dir_path, 'header_support_file.html'
     @footer_path      = File.join @tmp_dir_path, 'footer_support_file.html'
-  end
-  # clean cache
-  def clean_cache
-    PDFKit.send(:remove_const, 'Generator')
-    load './lib/pdfkit/generator.rb'
-  end
-  # precondition methods assurance
-  def directories_up_precondition
-    FileUtils.mkdir_p(@tmp_dir_path)
-    File.directory?(@tmp_dir_path).should be_true
-    FileUtils.mkdir_p(@default_dir_path)
-    File.directory?(@default_dir_path).should be_true
-  end
-  def directories_down_precondition
-    FileUtils.rm_rf(@default_dir_path)
-    File.directory?(@default_dir_path).should be_false
-    FileUtils.rm_rf(@tmp_dir_path)
-    File.directory?(@tmp_dir_path).should be_false
-  end
-  def files_up_precondition
-    File.exists?(@cover_path).should be_true
-    File.exists?(@header_path).should be_true
-    File.exists?(@footer_path).should be_true
-  end
-  # methods need for options_for_pdfkit and method print
-  def temporary_path
-    File.join('printer')
-  end
-  def create_temporary_folder
-    FileUtils.mkdir_p temporary_path
-  end
-  def destroy_temporary_folder
-    FileUtils.rm_rf temporary_path
-  end
-  def path_to_document_cover_html_file
-    File.join(temporary_path, 'cover.html')
-  end
-  def path_to_document_header_html_file
-    File.join(temporary_path, 'header.html')
-  end
-  def path_to_document_body_html_file
-    File.join(temporary_path, 'body.html')
-  end
-  def path_to_document_footer_html_file
-    File.join(temporary_path, 'footer.html')
-  end
-  def path_to_document_pdf
-    Pathname.new(File.join(temporary_path, 'document.pdf'))
-  end
-  def create_document_cover_html_file
-    File.open(path_to_document_cover_html_file, 'w') {|f| f.write(path_to_document_cover_html_file)}
-  end
-  def create_document_header_html_file
-    File.open(path_to_document_header_html_file, 'w') {|f| f.write(path_to_document_header_html_file)}
-  end
-  def create_document_body_html_file
-    File.open(path_to_document_body_html_file, 'w') {|f| f.write(path_to_document_body_html_file)}
-  end
-  def create_document_footer_html_file
-    File.open(path_to_document_footer_html_file, 'w') {|f| f.write(path_to_document_footer_html_file)}
   end
   describe "#default_directory_path" do
     before(:each) do # because we are testing class methods with cache ;)
---
+++ spec/generator_spec_helper.rb
@@ -0,0 +1,60 @@
+# clean cache
+def clean_cache
+  PDFKit.send(:remove_const, 'Generator')
+  load './lib/pdfkit/generator.rb'
+end
+# precondition methods assurance
+def directories_up_precondition
+  FileUtils.mkdir_p(@tmp_dir_path)
+  File.directory?(@tmp_dir_path).should be_true
+  FileUtils.mkdir_p(@default_dir_path)
+  File.directory?(@default_dir_path).should be_true
+end
+def directories_down_precondition
+  FileUtils.rm_rf(@default_dir_path)
+  File.directory?(@default_dir_path).should be_false
+  FileUtils.rm_rf(@tmp_dir_path)
+  File.directory?(@tmp_dir_path).should be_false
+end
+def files_up_precondition
+  File.exists?(@cover_path).should be_true
+  File.exists?(@header_path).should be_true
+  File.exists?(@footer_path).should be_true
+end
+# methods need for options_for_pdfkit and method print
+def temporary_path
+  File.join('printer')
+end
+def create_temporary_folder
+  FileUtils.mkdir_p temporary_path
+end
+def destroy_temporary_folder
+  FileUtils.rm_rf temporary_path
+end
+def path_to_document_cover_html_file
+  File.join(temporary_path, 'cover.html')
+end
+def path_to_document_header_html_file
+  File.join(temporary_path, 'header.html')
+end
+def path_to_document_body_html_file
+  File.join(temporary_path, 'body.html')
+end
+def path_to_document_footer_html_file
+  File.join(temporary_path, 'footer.html')
+end
+def path_to_document_pdf
+  Pathname.new(File.join(temporary_path, 'document.pdf'))
+end
+def create_document_cover_html_file
+  File.open(path_to_document_cover_html_file, 'w') {|f| f.write(path_to_document_cover_html_file)}
+end
+def create_document_header_html_file
+  File.open(path_to_document_header_html_file, 'w') {|f| f.write(path_to_document_header_html_file)}
+end
+def create_document_body_html_file
+  File.open(path_to_document_body_html_file, 'w') {|f| f.write(path_to_document_body_html_file)}
+end
+def create_document_footer_html_file
+  File.open(path_to_document_footer_html_file, 'w') {|f| f.write(path_to_document_footer_html_file)}
+end
