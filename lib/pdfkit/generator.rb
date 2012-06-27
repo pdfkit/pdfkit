fa653f88f114b9ebcd1b56ee37807954cc0937eb
Frank Pimenta <frankapimenta@gmail.com>
2012-06-27 13:36:29 +0200

    . generator: -self.options_for_pdf_kit - accepting document configuration and parts arguments only

--- lib/pdfkit/generator.rb
+++ lib/pdfkit/generator.rb
@@ -125,21 +125,20 @@ class PDFKit
         end
         # options used by pdfkit for create the document
         #
-        #  @param [String] _title_ document title
+        #  @param [Hash] _document_parts_ with the document different parts
         #  @param [Hash] _document_configurations_ with the document pages configurations
         #    - such as page size, margins, etc...
         #  @return [Hash] with the options to be set on to be generated pdf document
-        def options_for_pdf_kit(_title_, _document_configurations_, _document_parts_)
-          # TODO: check if _title_ can be retrieved from _document_parts_
+        def options_for_pdf_kit(_document_parts_, _document_configurations_)
           # inject the documents here
           _options = {}.merge(PDFKit.configuration.default_options)
           _options.merge!(_document_configurations_)
-          _document_parts = _document_parts_.nil? ?  temporary_file_paths : _document_parts_
+          _document_parts = _document_parts_[:header].instance_of?(Pathname) ? _document_parts_ : temporary_file_paths
           _options.merge!({
             :cover       => _document_parts[:cover],
             :header_html => _document_parts[:header],
             :footer_html => _document_parts[:footer],
-            :title       => _title_,
+            :title       => _document_parts_[:title]
           })
         end
         # return the document path to be used in the generation of the pdf file
