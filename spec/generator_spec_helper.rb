9e284e946b283cbba1ac0f21b61168c2c22fde4d
Frank Pimenta <frankapimenta@gmail.com>
2012-07-05 22:41:59 +0200

    % generator: spec_helper -> cache applied and use of ivar to hold path lonely dropped

--- spec/generator_spec_helper.rb
+++ spec/generator_spec_helper.rb
@@ -5,28 +5,28 @@ def clean_cache
 end
 # precondition methods assurance
 def directories_up_precondition
-  FileUtils.mkdir_p(@tmp_dir_path)
-  File.directory?(@tmp_dir_path).should be_true
-  FileUtils.mkdir_p(@default_dir_path)
-  File.directory?(@default_dir_path).should be_true
+  FileUtils.mkdir_p(temporary_directory_path)
+  File.directory?(temporary_directory_path).should be_true
+  FileUtils.mkdir_p(default_directory_path)
+  File.directory?(default_directory_path).should be_true
 end
 def directories_down_precondition
-  FileUtils.rm_rf(@default_dir_path)
-  File.directory?(@default_dir_path).should be_false
-  FileUtils.rm_rf(@tmp_dir_path)
-  File.directory?(@tmp_dir_path).should be_false
+  FileUtils.rm_rf(default_directory_path)
+  File.directory?(default_directory_path).should be_false
+  FileUtils.rm_rf(temporary_directory_path)
+  File.directory?(temporary_directory_path).should be_false
 end
 def files_up_precondition
-  File.exists?(@cover_path).should be_true
-  File.exists?(@header_path).should be_true
-  File.exists?(@footer_path).should be_true
+  File.exists?(path_to_temporary_document_cover_html_file).should be_true
+  File.exists?(path_to_temporary_document_header_html_file).should be_true
+  File.exists?(path_to_temporary_document_footer_html_file).should be_true
 end
 # methods need for options_for_pdfkit and method print
 def default_directory_path
-  Pathname.new File.join('documents')
+  @default_directory_path ||= Pathname.new 'documents'
 end
 def temporary_directory_path
-  Pathname.new File.join('printer')
+  @temporary_directory_path ||= Pathname.new 'pdfkit'
 end
 def create_temporary_folder
   FileUtils.mkdir_p temporary_directory_path
@@ -35,22 +35,22 @@ def destroy_temporary_folder
   FileUtils.rm_rf temporary_directory_path
 end
 def path_to_document_cover_html_file
-  Pathname.new File.join(temporary_directory_path, 'cover.html')
+  @path_to_document_cover_html_file ||= temporary_directory_path.join('cover.html')
 end
 def path_to_document_header_html_file
-  Pathname.new File.join(temporary_directory_path, 'header.html')
+  @path_to_document_header_html_file ||= temporary_directory_path.join('header.html')
 end
 def path_to_document_body_html_file
-  Pathname.new File.join(temporary_directory_path, 'body.html')
+  @path_to_document_body_html_file ||= temporary_directory_path.join('body.html')
 end
 def path_to_document_footer_html_file
-  Pathname.new File.join(temporary_directory_path, 'footer.html')
+  @path_to_document_footer_html_file ||= temporary_directory_path.join('footer.html')
 end
 def path_to_document_pdf
-  File.join(temporary_directory_path, 'document.pdf')
+  @path_to_document_pdf ||= temporary_directory_path.join('document.pdf')
 end
 def path_to_css
-  Pathname.new File.join(temporary_directory_path, 'license.css')
+  @path_to_css ||= temporary_directory_path.join('license.css')
 end
 def delete_default_directory
   FileUtils.rm_rf default_directory_path
