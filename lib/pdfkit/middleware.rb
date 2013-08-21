572c3d946379b8651892c885f9bcb9699194d3b0
Nathan Wiebe <nathan.wiebe@westernlife.com>
2013-08-21 15:26:15 -0500

    Allow controllers to set PDFKit-save-pdf header to cause rendered pdf to be saved in the filesystem

--- lib/pdfkit/middleware.rb
+++ lib/pdfkit/middleware.rb
@@ -21,6 +21,11 @@ class PDFKit
         body = PDFKit.new(translate_paths(body, env), @options).to_pdf
         response = [body]
 
+        if headers['PDFKit-save-pdf']
+          File.open(headers['PDFKit-save-pdf'], 'wb') { |file| file.write(body) }
+          headers.delete('PDFKit-save-pdf')
+        end
+
         # Do not cache PDFs
         headers.delete('ETag')
         headers.delete('Cache-Control')
