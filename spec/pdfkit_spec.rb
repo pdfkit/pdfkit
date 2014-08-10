c9a85a27d1bae69a12b2a6d08fc7b4ac4f162f75
Ian Cordasco <graffatcolmingov@gmail.com>
2014-08-09 19:55:07 -0500

    Rewrite the test from @mikefarah

--- spec/pdfkit_spec.rb
+++ spec/pdfkit_spec.rb
@@ -248,19 +248,19 @@ describe PDFKit do
       file_path = "/my/file/path.pdf"
       pdfkit = PDFKit.new('html', :page_size => 'Letter')
 
-      mockPdf = mock(Object)
-      mockPdf.should_receive(:puts)
-      mockPdf.should_not_receive(:gets) # do no read the contents when given a file path
-      mockPdf.should_receive(:close_write)
+      mock_pdf = double
+      expect(mock_pdf).to receive(:puts)
+      expect(mock_pdf).not_to receive(:gets) # do no read the contents when given a file path
+      expect(mock_pdf).to receive(:close_write)
       
 
-      IO.should_receive(:popen) do |args, mode, &block|
-        block.call(mockPdf)
+      expect(IO).to receive(:popen) do |args, mode, &block|
+        block.call(mock_pdf)
       end
 
-      File.should_receive(:size).with(file_path).and_return(50)
+      expect(::File).to receive(:size).with(file_path).and_return(50)
 
-      file = pdfkit.to_pdf(file_path)
+      pdfkit.to_pdf(file_path)
     end
 
     it "should generate a PDF of the HTML" do
