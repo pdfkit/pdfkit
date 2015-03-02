682058babe027991af6b14de8c93adf82452b597
Jo Cranford <jo.cranford@gmail.com>
2015-03-02 13:42:48 +1100

    Ensure regexp correctly matches relative root path
    Previously if there was a tag with href='/' followed by another tag with a relative URL, the contents of the second tag would not be replaced with an absolute URL. Added a test for this edge case and tweaked the regexp to fix.

--- lib/pdfkit/middleware.rb
+++ lib/pdfkit/middleware.rb
@@ -52,7 +52,7 @@ class PDFKit
     def translate_relative_paths(body, env)
       root = PDFKit.configuration.root_url || "#{env['rack.url_scheme']}://#{env['HTTP_HOST']}/"
       # Try out this regexp using rubular http://rubular.com/r/vmuGSkheuu
-      body.gsub(/(href|src)=(['"])\/([^\/]([^\"']*|[^"']*))['"]/, "\\1=\\2#{root}\\3\\2")
+      body.gsub(/(href|src)=(['"])\/([^\/"']([^\"']*|[^"']*))?['"]/, "\\1=\\2#{root}\\3\\2")
     end
 
     def translate_relative_protocols(body, env)
