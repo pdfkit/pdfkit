ca140da397b3ea69e10478503d50b2b62b52c0dc
aunger <amy.e.unger@gmail.com>
2015-05-04 21:22:34 -0500

    Add SimpleCov & Pry as development dependencies

--- Gemfile
+++ Gemfile
@@ -2,6 +2,11 @@ source 'https://rubygems.org'
 
 group :test do
   gem 'activesupport', '~> 3.0'
+  gem 'simplecov', require: false
+end
+
+group :development, :test do
+  gem 'pry'
 end
 
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 SPEC_ROOT = File.dirname(__FILE__)
 $LOAD_PATH.unshift(SPEC_ROOT)
 $LOAD_PATH.unshift(File.join(SPEC_ROOT, '..', 'lib'))
+require 'simplecov'
+SimpleCov.start
+
 require 'pdfkit'
 require 'rspec'
 require 'mocha'
