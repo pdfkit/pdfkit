70e947c072259d97b7279ff99bb053afbf03f837
aunger <amy.e.unger@gmail.com>
2015-05-16 10:23:49 -0500

    Filter specs from test coverage reports

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,9 @@ SPEC_ROOT = File.dirname(__FILE__)
 $LOAD_PATH.unshift(SPEC_ROOT)
 $LOAD_PATH.unshift(File.join(SPEC_ROOT, '..', 'lib'))
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_filter 'spec/'
+end
 
 require 'pdfkit'
 require 'rspec'
