056bbc5fe6af66cb657c2a13ae3af293fee0a499
Gary Greyling <gary@mpowered.co.za>
2017-09-28 10:25:00 +0200

    enable pdfkit meta tags with nil content

--- lib/pdfkit/pdfkit.rb
+++ lib/pdfkit/pdfkit.rb
@@ -89,7 +89,7 @@ class PDFKit
     content.scan(/<meta [^>]*>/) do |meta|
       if meta.match(/name=["']#{PDFKit.configuration.meta_tag_prefix}/)
         name = meta.scan(/name=["']#{PDFKit.configuration.meta_tag_prefix}([^"']*)/)[0][0].split
-        found[name] = meta.scan(/content=["']([^"'\\]+)["']/)[0][0]
+        found[name] = meta.scan(/content=["']([^"'\\]+)["']/)[0].try(:fetch, 0)
       end
     end
 
