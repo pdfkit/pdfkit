8ba8d6ed3ab289177db6be95fa506ffc591c30ab
Vlad Mencl <vladimir.mencl@reannz.co.nz>
2022-10-12 23:11:15 +1300

    rspec: add tests for mixed case Content-Type header support
    As rack 3.x outright rejects mixed case headers, this test has to be marked
    as pending - but passes (without the pending flag) with rack 2.x.

--- spec/middleware_spec.rb
+++ spec/middleware_spec.rb
@@ -422,6 +422,28 @@ describe PDFKit::Middleware do
       end
     end
 
+    describe "content type header" do
+      before { mock_app }
+
+      context "lower case" do
+        specify "header gets correctly updated" do
+          get 'http://www.example.org/public/test.pdf'
+          expect(last_response.headers["content-type"]).to eq("application/pdf")
+        end
+      end
+
+      context "mixed case" do
+        let(:headers) do
+          {'Content-Type' => "text/html"}
+        end
+
+        specify "header gets correctly updated", pending: "this test only applies to rack 2.x and is rejected by rack 3.x" do
+          get 'http://www.example.org/public/test.pdf'
+          expect(last_response.headers["Content-Type"]).to eq("application/pdf")
+        end
+      end
+    end
+
     describe "remove .pdf from PATH_INFO and REQUEST_URI" do
       before { mock_app }
 
